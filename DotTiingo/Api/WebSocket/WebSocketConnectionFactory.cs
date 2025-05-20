0706574a22f95bf6a613dfed623b0cbc4bdb10ef
grovre <landonvee111@gmail.com>
2025-05-19 20:20:17 -0500

    Add web socket auth timeout as workaround for bad Tiingo backend implementation

--- DotTiingo/Api/WebSocket/WebSocketConnectionFactory.cs
+++ DotTiingo/Api/WebSocket/WebSocketConnectionFactory.cs
@@ -24,6 +24,7 @@ internal class WebSocketConnectionFactory(WebSocketAuthorization wsAuth)
         var cws = new ClientWebSocket();
         await cws.ConnectAsync(new(baseUrl), cancelToken);
         var conn = new WebSocketConnection(cws, cancelToken);
+        var authTimeoutCheck = new AuthTimeoutCheck(TimeSpan.FromSeconds(10));
         EventHandler<AbstractResponse> authCheck = (_, response) =>
         {
             if (response is not UtilityResponse utilityResponse)
@@ -38,9 +39,75 @@ internal class WebSocketConnectionFactory(WebSocketAuthorization wsAuth)
                 throw new Exception(
             $"WebSocket authorization response was not of type 'I' (info) but {utilityResponse.MessageType}");
         };
+        authCheck += (_, _) => authTimeoutCheck.TimedOut = false;
         authCheck += (sender, _) => ((ITiingoWebSocketConnection)sender!).OnResponseReceived -= authCheck;
         conn.OnResponseReceived += authCheck;
         await cws.SendAsync(authBytes, WebSocketMessageType.Text, true, cancelToken);
+        await authTimeoutCheck.CreateAuthCheckTask();
         return conn;
+    }
+}
+
+/// <summary>
+/// Provides a mechanism to enforce a timeout for WebSocket authorization.
+/// Throws a <see cref="TimeoutException"/> if authorization is not completed within the specified time span.
+/// </summary>
+file sealed class AuthTimeoutCheck
+{
+    private readonly CancellationTokenSource _cancelTokenSource;
+    private bool _timedOut;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the authorization has timed out.
+    /// Setting this to <c>false</c> cancels the timeout check.
+    /// </summary>
+    public bool TimedOut
+    {
+        get => _timedOut;
+        set
+        {
+            _timedOut = value;
+            if (!value)
+                _cancelTokenSource.Cancel();
+        }
+    }
+
+    /// <summary>
+    /// Gets the time span to wait before timing out the authorization.
+    /// </summary>
+    public TimeSpan TimeSpan { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AuthTimeoutCheck"/> class with the specified timeout duration.
+    /// </summary>
+    /// <param name="timeSpan">The duration to wait before timing out.</param>
+    public AuthTimeoutCheck(TimeSpan timeSpan)
+    {
+        _cancelTokenSource = new();
+        TimedOut = true;
+        TimeSpan = timeSpan;
+    }
+
+    /// <summary>
+    /// Starts the authorization timeout check. Throws a <see cref="TimeoutException"/> if the timeout elapses before authorization completes.
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    /// <exception cref="TimeoutException">Thrown if the timeout elapses before authorization completes.</exception>
+    public async Task CreateAuthCheckTask()
+    {
+        if (!TimedOut || _cancelTokenSource.IsCancellationRequested)
+            return;
+
+        try
+        {
+            await Task.Delay(TimeSpan, _cancelTokenSource.Token);
+        }
+        catch (TaskCanceledException)
+        {
+            // Task was canceled, do nothing
+        }
+
+        if (TimedOut)
+            throw new TimeoutException("WebSocket authorization timed out. No responses received.");
     }
 }
