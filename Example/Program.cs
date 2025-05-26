0cab8fc55c67e65d2178d669496dae4ecee85beb
grovre <landonvee111@gmail.com>
2025-05-26 00:09:23 -0500

    better example

--- Example/Program.cs
+++ Example/Program.cs
@@ -19,14 +19,10 @@ var tiingoToken = cfg["tiingo_token"]
 using var httpClient = new HttpClient();
 var client = new TiingoClient(httpClient, tiingoToken);
 
-using var conn = await client.WebSocket.Forex.Connect(ForexThresholdLevel.TopOfBook, CancellationToken.None);
+using var conn = await client.WebSocket.Crypto.Connect(CryptoThresholdLevel.Trade, CancellationToken.None);
 
-Console.OutputEncoding = Encoding.UTF8;
-var plot = new Plot(Console.WindowWidth, Console.WindowHeight);
-plot.Ticks.Labels.Format = "N5";
-var xs = new LinkedList<double>();
-var ys = new LinkedList<double>();
-var start = DateTime.Now;
+double[] xs = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+double[] ys = Enumerable.Repeat(0.0, xs.Length).ToArray();
 var @lock = new Lock();
 
 conn.OnResponseReceived += (_, r) =>
@@ -34,41 +30,34 @@ conn.OnResponseReceived += (_, r) =>
     if (r is not DataResponse dr)
         return;
 
-    if (dr.Data is not ForexQuoteUpdate fqu)
+    if (dr.Data is not CryptoTradeUpdate ctu)
         return;
 
-    if (fqu.Ticker != "eurusd")
+    var lastSize = ctu.LastSize;
+    while (lastSize < 1.0)
+        lastSize *= 10;
+
+    var d = lastSize.ToString("N0")[0] - '0';
+
+    if (d == 0)
         return;
 
-    var elapsedSecs = (DateTime.Now - start).TotalSeconds;
     lock (@lock)
     {
-        xs.AddLast(elapsedSecs);
-        ys.AddLast(fqu.AskPrice);
+        ys[d - 1]++;
     }
 };
 
 await Task.Run(() =>
 {
-    var lastXs = 0;
-    var lastYs = 0;
     while (true)
     {
         Thread.Sleep(1000);
-        plot.Series.Clear();
+        Console.OutputEncoding = Encoding.UTF8;
+        var plot = new Plot(Console.WindowWidth, Console.WindowHeight);
+        plot.Ticks.Labels.Format = "N0";
         lock (@lock)
         {
-            Debug.Assert(xs.Count == ys.Count);
-
-            if (xs.Count == 0 || ys.Count == 0)
-                continue;
-
-            if (lastXs == xs.Count && lastYs == ys.Count)
-                continue;
-
-            lastXs = xs.Count;
-            lastYs = ys.Count;
-
             plot.AddSeries(xs, ys);
             plot.Draw();
             Console.Clear();
@@ -76,5 +65,3 @@ await Task.Run(() =>
         }
     }
 });
-
-await Task.Delay(-1);
